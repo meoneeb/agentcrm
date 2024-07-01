f394821fc5cd6eed4e7da0e92b4691fee2856ac3
Oneeb Faisal <oneebfaisal@gmail.com>
2024-07-02 00:15:16 +0500

    add smartpasses link

--- src/data/agent.js
+++ src/data/agent.js
@@ -12,7 +12,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2023/09/Kelli-Cordes-Marketing-Director-1.jpg",
     vcard: "/contact.vcf",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/azwtdwsk9h-t4667f061a6b4f3",
     action: [
       {
         label: "Text",
@@ -38,7 +38,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2019/08/Chuck-Scott-General-Manager-e1566515611253.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/mlto894120xsh667f227034ce4",
     action: [
       {
         label: "Text",
@@ -64,7 +64,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2020/11/Jason-11-2020-scaled-e1620168169673.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/6ffavuzecnm-96682d301b5021",
     action: [
       {
         label: "Text",
@@ -90,7 +90,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2024/02/RORY-scaled-e1708458559484.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/-8snb00ymel0q6682d848a6875",
     action: [
       {
         label: "Text",
@@ -116,7 +116,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2024/06/cam-2-scaled-e1717689472940.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/qrykl4lc9bjyw6682daf198663",
     action: [
       {
         label: "Text",
@@ -142,7 +142,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2018/11/Steffani-Gilbert-Parts-Manager-e1541193620412.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/s_wodefnb5yk-6682e11014416",
     action: [
       {
         label: "Text",
@@ -168,7 +168,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2018/11/Marty-Grindstaff-sales-e1541194552521.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/ysgj9ltwsm86h6682e3354e62b",
     action: [
       {
         label: "Text",
@@ -194,7 +194,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2024/02/TINA-e1708458826645.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/kjdlf65yivl3d6682e698ce941",
     action: [
       {
         label: "Text",
@@ -220,7 +220,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2019/08/Kyle-Chapman-1-e1566516252198.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/ash0muti7e_7z6682e89baa844",
     action: [
       {
         label: "Text",
@@ -246,7 +246,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2024/02/COLTON-scaled-e1708458977230.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/o7ldchdt9udbr6682ea8cd9b3a",
     action: [
       {
         label: "Text",
@@ -272,7 +272,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2020/11/Randy-scaled.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/36pw_ndryqe516682ecb26e76a",
     action: [
       {
         label: "Text",
@@ -298,7 +298,7 @@ export const agentArr = [
     img: "https://edgeperformancesports.com/wp-content/uploads/sites/45/2018/11/Javier-Cardoza-Finance-Manager-e1566517889878.jpg",
     vcard: "/api/download-vcard",
     smartpass:
-      "https://smartpass.i1smartmarketing.com/en/p/_z0eif68y-1ns6675e4e8c115d",
+      "https://smartpass.i1smartmarketing.com/en/p/s5k6z521gjlg86682ef347f58e",
     action: [
       {
         label: "Text",
