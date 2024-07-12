dcfbefc7e2ffa9034f4764b89d7992643f556631
Oneeb Faisal <oneebfaisal@gmail.com>
2024-07-12 21:04:12 +0500

    add clickup link

---
+++ src/app/admin/tasks/page.js
@@ -0,0 +1,14 @@
+export default function page() {
+  return (
+    <div>
+      <iframe
+        class="clickup-embed"
+        src="https://sharing.clickup.com/9018321303/l/h/6-901802484972-1/eb28eb60b79787f"
+        onwheel=""
+        width="100%"
+        height="700px"
+        // style={{background: transparent; border: 1px solid #ccc;"
+      ></iframe>
+    </div>
+  );
+}
