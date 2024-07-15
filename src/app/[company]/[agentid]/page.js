89dacbc09d4d9ca1154779cc6cb116958bcc9ba9
Oneeb Faisal <oneebfaisal@gmail.com>
2024-07-16 03:18:36 +0500

    add company favicon

--- src/app/[company]/[agentid]/page.js
+++ src/app/[company]/[agentid]/page.js
@@ -26,12 +26,12 @@ export default function AgentPage({ params }) {
 
   const fullname = `${agentProfile.firstname} ${agentProfile.lastname}`;
   const companyProfile = agentProfile.companyDetails;
-
+  const favicon = companyProfile.favicon || "/icons/fav.png";
   return (
     <>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/icons/fav.png" />
+        <link rel="icon" href={favicon} />
         <meta charSet="utf-8" />
         <meta name="description" content="" />
         <meta property="og:title" content={fullname} key="ogtitle" />
--- src/app/db/agents/leads/page.js
+++ src/app/db/agents/leads/page.js
@@ -2,6 +2,9 @@ export default function page() {
   return (
     <div className="flex p-4 w-full">
       <h1 className="text-2xl font-bold">Leads</h1>
+      <table>
+        
+      </table>
     </div>
   );
 }
