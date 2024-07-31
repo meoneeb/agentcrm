ae14557208a550f13fb6c951345607561b0da107
Oneeb Faisal <oneebfaisal@gmail.com>
2024-07-31 20:06:39 +0500

    revert

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import RedirectSmartMarketing from "@/components/home/RedirectSmartMarketing";
 export default function Home() {
   return (
     <main className="min-h-screen w-screen bg-zinc-900">
-      {/* <RedirectSmartMarketing /> */}
-      <a href="/figics/meoneeb">go to meoneeb</a>
+      <RedirectSmartMarketing />
+     
     </main>
   );
 }
