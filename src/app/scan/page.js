f218aee6809107bc9b33ac601b0b61d76f3ea2b8
meoneeb <oneebfaisal@gmail.com>
2025-02-20 23:46:02 +0500

    scan

---
+++ src/app/scan/page.js
@@ -0,0 +1,10 @@
+import CameraScanner from "@/components/vehicle/new/Scanner";
+
+export default function ScannerPage() {
+  return (
+    <div className="flex flex-col items-center min-h-screen p-4">
+      <h1 className="text-2xl font-bold">Scan Text using Camera</h1>
+      <CameraScanner />
+    </div>
+  );
+}
